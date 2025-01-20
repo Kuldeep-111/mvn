e7527dbe6546e1c9350f317f02a8e56f467489cc
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-01-20 17:07:35 +0530

    one view with elevation

--- src/frontend/components/MicroPage/MicroElevation.jsx
+++ src/frontend/components/MicroPage/MicroElevation.jsx
@@ -91,7 +91,7 @@ export default function MicroElevation({ elevationData, onLoadComplete }) {
         <div className="card center" onClick={() => setIndex(index)}>
           {/* Wrapping image in AnImage component for animation */}
           <AnImage ref={(el) => (imageDivRefs.current[index] = el)}>
-            <img src={image.assets.mobile} alt={`mvn elevation ${index}`} className='img-fluid elevation-section-img d-none d-md-block' onLoad={handleImageLoad} />
+            <img src={image.assets.desktop} alt={`mvn elevation ${index}`} className='img-fluid elevation-section-img d-none d-md-block' onLoad={handleImageLoad} />
             <img src={image.assets.mobile} alt={`mvn elevation ${index}`} className='img-fluid elevation-section-img d-md-none' />
             <Watermark className={image.watermark} />
           </AnImage>
